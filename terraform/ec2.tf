6a776700ea82f4338a20835179c7843ce68d1d0d
zack hee <zack.hee@handshakes.com.my>
2024-04-19 23:27:02 +0800

    fix: instance's ami

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test_instance" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.nano"
+  ami           = "ami-0dc44e17251b74325"
+  instance_type = "t2.micro"
   tags = {
     Name = "test_instance"
   }
