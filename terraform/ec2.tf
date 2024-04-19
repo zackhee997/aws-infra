d8de47c5b9a1b71cb632385918df3764b3e8e24a
zack hee <zack.hee@handshakes.com.my>
2024-04-19 23:31:22 +0800

    feat: add subent

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -14,3 +14,12 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
