22a0241462c471735179f9088d2ade6fdbeaa9b0
zack hee <zack.hee@handshakes.com.my>
2024-04-19 23:21:26 +0800

    feat: add hashicorp provider and state key

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,12 @@
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
   backend "s3" {
     bucket = "state-bucket-tf"
-    # key    = "path/to/my/key"
+    key    = "terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
