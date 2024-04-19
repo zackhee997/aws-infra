1e69f6144e13e8282b63172be24af71f50796adc
zack hee <zack.hee@handshakes.com.my>
2024-04-19 23:28:33 +0800

    feat: add vpc

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,16 @@
-resource "aws_instance" "test_instance" {
-  ami           = "ami-0dc44e17251b74325"
-  instance_type = "t2.micro"
+# resource "aws_instance" "test_instance" {
+#   ami           = "ami-0dc44e17251b74325"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "test_instance"
+#   }
+# }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
   tags = {
-    Name = "test_instance"
+    Name = "main"
   }
 }
