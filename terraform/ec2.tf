a9fca294f2887eb597c05f8d2fe0d8bae895f653
zack hee <zack.hee@handshakes.com.my>
2024-04-20 15:40:59 +0800

    refactor: remove vpc and subnet

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,20 +6,20 @@
 #   }
 # }
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+# resource "aws_vpc" "main" {
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "main"
-  }
-}
+#   tags = {
+#     Name = "main"
+#   }
+# }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+# resource "aws_subnet" "main" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "public-subnet-1"
-  }
-}
+#   tags = {
+#     Name = "public-subnet-1"
+#   }
+# }
